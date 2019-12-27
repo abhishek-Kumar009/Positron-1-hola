8d5854a9e880d11915e93313b672dc1148c78f72
Abhishek <kumar.bits009@gmail.com>
2019-12-27 17:59:30 +0530

    commiting

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(passport.session());
 
 app.use(logger('dev'));
 
-app.locals.baseURL = "18.191.24.211/"
+app.locals.baseURL = "http://localhost:3000/"
 
 app.use(require('./routes/loginmiddlewares').addTemplate);
 app.use(require('./routes/search'));
