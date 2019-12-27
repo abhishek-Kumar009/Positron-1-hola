d18374fa712a41047df0b72bb469ab2028eb09cb
Abhishek <kumar.bits009@gmail.com>
2019-12-27 19:38:01 +0530

    domain

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use(passport.session());
 
 app.use(logger('dev'));
 
-app.locals.baseURL = "http://askspyra.com"
+app.locals.baseURL = "askspyra.com";
 
 app.use(require('./routes/loginmiddlewares').addTemplate);
 app.use(require('./routes/search'));
