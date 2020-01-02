7519b12588529bd0286b9b7aee7b9f7ee935e29a
Abhishek <kumar.bits009@gmail.com>
2020-01-02 12:06:02 +0530

    changed custom.css

--- routes/loginL.js
+++ routes/loginL.js
@@ -1,26 +1,34 @@
 'use strict'
 
-const express=require('express');
+const express = require('express');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-const crypto=require('crypto');
-var User=require('./models').User;
-const ErrorHandler=require('../_helpers/error-handler');
+const crypto = require('crypto');
+var User = require('./models').User;
+const ErrorHandler = require('../_helpers/error-handler');
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
+  function (username, password, done) {
     console.log("reached inside local strategy");
-    const hash=crypto.createHash('sha256');
+    const hash = crypto.createHash('sha256');
     hash.update(password);
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
+    User.findOne({
+      username: username
+    }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
       if (!user) {
         console.log("User Not Found");
-        return done(null, false, { message: 'Incorrect username.' });
+        return done(null, false, {
+          message: 'Incorrect username.'
+        });
       }
-      if (!user.password==hash.digest('hex')) {
+      if (!user.password == hash.digest('hex')) {
         console.log("Password entered is correct");
-        return done(null, false, { message: 'Incorrect password.' });
+        return done(null, false, {
+          message: 'Incorrect password.'
+        });
       }
       console.log('user found about to serialize');
       return done(null, user);
@@ -28,47 +36,42 @@ passport.use(new LocalStrategy(
   }
 ));
 
-passport.serializeUser(function(user, done) {
-    console.log('serializing user');
-    done(null, user._id);
+passport.serializeUser(function (user, done) {
+  console.log('serializing user');
+  done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+  console.log('deserializing user')
+  User.findById(id, function (err, user) {
+    done(err, user);
   });
-  
-passport.deserializeUser(function(id, done) {
-    console.log('deserializing user')
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
+});
 const router = express.Router();
 
-router.post('/auth/local',function(req, res, next) 
-{
+router.post('/auth/local', function (req, res, next) {
   console.log('about to start local strategy')
-  passport.authenticate('local', function(err, user, info) 
-  {
+  passport.authenticate('local', function (err, user, info) {
     console.log('Authentication function completed, LETS see the result');
-    if(err)
-    {
+    if (err) {
       console.log("some error while user tried to login")
-      return ErrorHandler(err,req,res);
-    }
-    else if(user==false)
-    {
+      return ErrorHandler(err, req, res);
+    } else if (user == false) {
       console.log('user not registered yet');
       return res.redirect('/auth/login');
-    }
-    else
-    {
-      req.login(user, function(err) {
-        if (err) { return ErrorHandler(err,req,res); }
+    } else {
+      req.login(user, function (err) {
+        if (err) {
+          return ErrorHandler(err, req, res);
+        }
         //return res.json(user);
         req.session.user = user;
         res.locals.user = user;
         return res.redirect('../home');
       });
-      
+
     }
   })(req, res, next);
 });
 
-module.exports=router;
+module.exports = router;
